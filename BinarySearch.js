a5428cd927f200401b75e94ee0df63857c6ecf65
tanvir.kour <tanvir.kour@codenicely.in>
2024-04-28 22:46:44 +0530

    Binary Search

---
+++ BinarySearch.js
@@ -0,0 +1,23 @@
+// Binary Search
+// Time complexity is
+// find the index of 0
+let arr = [39, 784, 0, 72, 7, 46]
+let target = 46;
+let length = arr.length
+let start = 0
+let end =length - 1
+
+while (start <= end) {
+    middle = Math.floor((start + end) / 2)
+    if (target === arr[middle]) {
+        console.log("The target is at index:", middle);
+        break;
+    }
+    else if (target < arr[middle]) {
+        end = middle -1
+    }   
+    else if (target > arr[middle]) {
+        start = middle + 1
+    }
+}
+// console.log("the target is on", targetIndex)
