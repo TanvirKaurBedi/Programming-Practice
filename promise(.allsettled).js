637df335c5fdf3a7b4524c0db10c9e5ce0893182
tanvir.kour <tanvir.kour@codenicely.in>
2024-04-30 21:41:02 +0530

    promise.allsettled

--- promise(.allsettled).js
+++ promise(.allsettled).js
@@ -1,5 +1,5 @@
-// what is promise.all?
-// promise.all is a method in javascript that takes an array or iterables of promises as input and returns a single promise .This returned promise resolves when all of the promises have resolved  or rejects if any of the input promises have been rejected 
+// what is promise.allsettled?
+// promise.allsetlled is a method in javascript that takes an array or iterables of promises as input and returns a single promise . This returned promise resolves after all of the input promises have settled means either resolved or rejected.The returned Promise resolves with an array of objects representing the outcome of each input promise.
 const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("p1 success")
@@ -15,6 +15,6 @@ const p3 = new Promise((resolve, reject) => {
         reject("P3 ")
     }, 5000)
 })
-Promise.all([p1, p2, p3])
+Promise.allSettled([p1, p2, p3])
     .then((res) => { console.log(res) })
     .catch((err) => { console.error(err) })
