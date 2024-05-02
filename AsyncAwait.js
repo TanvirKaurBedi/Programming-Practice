e81daed7846b9c4a4f9bf67e9dea10e49ef40ef2
tanvir.kour <tanvir.kour@codenicely.in>
2024-05-02 14:21:12 +0530

    asyn await eg

---
+++ AsyncAwait.js
@@ -0,0 +1,21 @@
+// what is async await ?
+
+// Async => The async keyword is used to declare that a function returns a promise . when you prepend 'async' to a function declaration , it automatically wraps the returned value in a resolved promise . 
+
+// Await => The await keyword can only be used inside an async function. It pauses the execution of the async function until the promise is resolved.Essentially it waits for the promise to be resolved and then returns the value.
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("api is resolved")
+    }, 1000)
+})
+
+ function handlePromise() {
+    let result =  p1
+    console.log("result", result)
+    console.log("apppp")
+}
+
+handlePromise()
+
+
